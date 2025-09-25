2a539ee96ec14f138abea972618cba4022357bf3
Antony Albuquerque <118316078+AlbuquerquexD@users.noreply.github.com>
2025-09-24 22:59:49 -0300

    add repository

---
+++ src/main/java/com/antony/todosimple/respositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.antony.todosimple.respositories;
+
+import com.antony.todosimple.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
